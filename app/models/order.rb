8a88e5027e260cf9102735cad79ff434218d99b3
mojomajor <info@redroostermobile.com>
2013-12-20 18:53:21 +0100

    pay_types moved from constant to  database

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,11 @@ class Order < ActiveRecord::Base
 
 	has_many :line_items, dependent: :destroy 
 	validates :name, :address, :email, presence: true
-	validates :pay_type, inclusion: PAYMENT_TYPES
-	
+	#validates :pay_type, inclusion: PAYMENT_TYPES
+	belongs_to :pay_type
+	validates :pay_type_id, presence: true
+
+
 	
 
 	def add_line_items_from_cart(cart)  
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,18 @@ Product.create(
 )
 Product.create(
 	title: 'Some Other Product',
-	description: 'even better that hip hopößé',
+	description: '<p>even better that hip hopößé</p>',
 	image_url: 'http://www.nasa.gov/sites/default/files/images/iBook_logo.png',
 	price: '49.95'
 )
+
+PayType.delete_all
+
+Order.delete_all
+LineItem.delete_all
+
+PayType.create(:name => "Check")
+PayType.create(:name => "Credit card")
+PayType.create(:name => "Purchase order")
 
 #http://www.nasa.gov/sites/default/files/images/iBook_logo.png
